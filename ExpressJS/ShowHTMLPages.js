fc82a5251a318d98c1a68b78dab061a49f0a9032
CodeBurner <codeburner0@gmail.com>
2023-06-03 23:51:58 +0530

    updated

--- ExpressJS/ShowHTMLPages.js
+++ ExpressJS/ShowHTMLPages.js
@@ -3,3 +3,7 @@ const app=express();
 const path=require('path')
 const dirname=path.join(__dirname,'public')
 app.use(express.static(dirname)).listen(3000)
+
+app.get('/about',(req,res)=>{
+    res.sendFile(dirname+"/About.html")
+})
