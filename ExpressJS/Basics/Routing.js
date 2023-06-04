9162fba5027393a05cd6bf51fe7a1a1ced9181cb
CodeBurner <codeburner0@gmail.com>
2023-06-04 08:38:29 +0530

    404 page not found

--- ExpressJS/Basics/Routing.js
+++ ExpressJS/Basics/Routing.js
@@ -1,11 +1,15 @@
 const express=require('express')
 const app=express();
-
+const path=require('path')
+const dirname=path.join(__dirname,"../public")
+console.log(dirname)
 app.get("",(req,res)=>{
     res.send('<h1>This is Home Page.</h1><a href="/about">Go to About</a>');
 })
 app.get("/about",(req,res)=>{
     res.send('<h1>This is About Page.</h1><a href="/">Go to Home</a>');
 })
-
+app.get("*",(req,res)=>{
+    res.sendFile(`${dirname}/404.html`)
+})
 app.listen(3000);
