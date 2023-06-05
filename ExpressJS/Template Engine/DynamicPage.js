43fe3c83eda63d0c7b464f2b8c2b3907b98ba3f4
CodeBurner <codeburner0@gmail.com>
2023-06-05 20:45:14 +0530

    Dynamic pages

---
+++ ExpressJS/Template Engine/DynamicPage.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const user = {
+  name: "Ankit Anand",
+  email: "ankit@gmail.com",
+  city: "Mathura",
+};
+app.set("view engine", "ejs");
+
+app.get("/about", (req, res) => {
+  res.render("about", { user });
+});
+
+app.listen(3000, () => [console.log("server is started")]);
