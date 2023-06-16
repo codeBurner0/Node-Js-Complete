4013cb252ffe8b5e3f18fc5eede72866cbe97d86
CodeBurner <codeburner0@gmail.com>
2023-06-16 16:50:21 +0530

    application middleware

---
+++ ExpressJS/Middleware/Application.js
@@ -0,0 +1,26 @@
+const express=require('express')
+const app=express();
+
+
+const reqFilter=(req,res,next)=>{
+    if(!req.query.age){
+        res.send("Enter Your Age ");
+    }
+    else if(req.query.age<18){
+        res.send("You are not allowed to enter, your age is not 18+")
+    }else{
+        next();
+    }
+    
+}
+app.use(reqFilter)
+app.get('/',(req,res)=>{
+    res.send("Welcom to home Screen");
+})
+app.get('/user',(req,res)=>{
+    res.send("Welcom to user Screen");
+
+})
+app.listen(5000,()=>[
+    console.log("server is started")
+])
